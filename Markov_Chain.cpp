b3b678d9f211bd5ef694fc63a52263f542e64583
AndrewYongZhenNing <ayzn95@gmail.com>
2017-06-24 11:30:18 +0100

    Branch initialising problem continues

--- Markov_Chain.cpp
+++ Markov_Chain.cpp
@@ -8,114 +8,113 @@
 
 #include "Markov_Chain.hpp"
 
-Markov_Chain::Markov_Chain(std::vector<double> currentPars, std::vector<double> coef, int nstep, std::string filename){
-    
-    
-    
+Markov_Chain::Markov_Chain(std::vector<double> &obj_pars, std::vector<std::string> &parsName, int nstep, std::string filename){
+
     steps = nstep;
-    file = new TFile(filename.c_str(),"RECREATE");
-    outputtree = new TTree("outputtree","");
-//    outputtree->Branch("Energy", &currentPars[0], "Energy/D");//0
-//    outputtree->Branch("DM2",&currentPars[1],"DM2/D");//1
-//    outputtree->Branch("Theta23",&currentPars[2],"Theta23/D");//2
-//    outputtree->Branch("Theta13",&currentPars[3],"Theta13/D");//3
-//    outputtree->Branch("dm2",&currentPars[4],"dm2/D");//4
-//    outputtree->Branch("Theta12",&currentPars[5],"Theta12/D");//5
-//    outputtree->Branch("Delta",&currentPars[6],"Delta/D");//6
-//    outputtree->Branch("n_series",&currentPars[7],"n_series/D");//7
-//    outputtree->Branch("Beta",&currentPars[8],"Beta/D");//8
-    
-    
-    outputtree->Branch("first",&coef[0],"first/D");//9
-    outputtree->Branch("second",&coef[1],"second/D");//10
-    outputtree->Branch("third",&coef[2],"third/D");//11
-    outputtree->Branch("fourth",&coef[3],"fourth/D");//12
-    outputtree->Branch("fifth",&coef[4],"fifth/D");//13
-    outputtree->Branch("sixth",&coef[5],"sixth/D");//14
-    outputtree->Branch("seventh",&coef[6],"seventh/D");//15
-    outputtree->Branch("eigth",&coef[7],"eigth/D");//16
-    outputtree->Branch("ninth",&coef[8],"ninth/D");//179
-    outputtree->Branch("LLH",&currentLLH,"LLH/D");
-    
-    outputtree->Fill();
-    
+    file = new TFile(filename.c_str(),"RECREATE");//
+    tree = new TTree("tree","");
+
+    double width_factor = 100; // default width factor
+
+    for(int j = 0; j < parsName.size();j++){
+      branchName.push_back(parsName[j]+"/D");
+      std::cout << branchName[j] << std::endl;
+    }
+
+    for(int i = 0; i<obj_pars.size(); i++){
+      //fills the branch
+      // TString temp = Form("parsName[%i]/D",i);
+      TString temp = branchName[i].c_str();
+      // tree->Branch(parsName[i].c_str(), obj_pars[i], "hi/D");
+      tree->Branch(parsName[i].c_str(), obj_pars[0],Form("parsName[%i]/D",i) ); // Siobhan: although doens't work, Form() might be useful for future
+      //initialising width vector; can be fine-tuned with set_width
+      // width.push_back(obj_pars[i]/width_factor);
+    }
+
+    // std::string bee = "Hi";
+    // tree->Branch(parsName[0].c_str(), obj_pars[0] , bee.c_str());
+
+
+    tree->Fill();
+
     for( int i = 0; i<=8; i++){
         pars.push_back(false);
-        coef_bool.push_back(true);
+        // coef_bool.push_back(true);
         proposedPars.push_back(0);
-        proposedcoef.push_back(0);
+        // proposedcoef.push_back(0);
          // populates the proposedPars vector
-        
+
     }
-    
-    double width_factor = 100; // default width factor
-    
+
     rnd  = new TRandom3(0);
-    
+
     //initialising width vector; can be fine-tuned with set_width
-//    width.push_back(0); // energy : 0
-//    width.push_back(2.4e-3/width_factor);//DM2 : 1
-//    width.push_back(0.5/width_factor); //theta23 : 2
-//    width.push_back(0.025/width_factor); //theta13 : 3
-//    width.push_back(7.6e-3/width_factor); //dm2 : 4
-//    width.push_back(0.312/width_factor); //theta12 : 5
-//    width.push_back(width_factor); // deltacp : 6
-//    width.push_back(10); // n : 7
-//    width.push_back(1/width_factor); // beta : 8
-    width.push_back(1.00043/width_factor);
-    width.push_back(-0.00513335/width_factor);
-    width.push_back(-0.81076/width_factor);
-    width.push_back(-0.0217913/width_factor);
-    width.push_back(0.223944/width_factor);
-    width.push_back(0.0340821/width_factor);
-    width.push_back(-0.0569585/width_factor);
-    width.push_back(0.0133551/width_factor);
-    width.push_back(-0.000970247/width_factor);
-    
-    
-    
-    
+    for(int k = 0; k < obj_pars.size(); k++){
+      width.push_back(obj_pars[k]/width_factor);
+    }
+   //E=0 : 0
+  //DM2=2.4e-3 : 1
+  //theta23=0.5 : 2
+  //theta13=0.025 : 3
+  //dm2=7.6e-3 : 4
+  //theta12=0.312 : 5
+  //deltacp=0 : 6
+  //n=10 : 7
+  //beta=1 : 8
+  //coef1=1.00043 : 9
+  //coef2=-0.00513335 : 10
+  //coef3=-0.81076 : 11
+  //coef4=-0.0217913 : 12
+  //coef5=0.223944 : 13
+  //coef6=0.0340821 : 14
+  //coef7=-0.0569585 : 15
+  //coef8=0.0133551 : 16
+  //coef9=-0.000970247 : 17
+
+
+
+
 }
 
 Markov_Chain::~Markov_Chain(){};
 
-void Markov_Chain::startMH(std::vector<double> currentPars, Nu_Fitter* oscObject){
-    
-    
-    
+void Markov_Chain::startMH(std::vector<double>& currentPars, Nu_Fitter* oscObject){
+
+
+
     currentLLH = oscObject->getLLH();
-    
+
     for(int i=0; i<steps; i++){
-        
+
         if(i%1000==0 || i==0){
-            
+
             if(i==0){
                 std::cout << "Initialising random walk..." << std::endl;
             }
-            
+
             else{
                 std::cout << "Running random walk..." << std::endl;
             }
-            
+
         }
-        
+
         for(int j=0; j<currentPars.size(); j++) {
-            
+
             if(pars[j]){
-                
+
                 proposedPars[j]=rnd->Gaus(currentPars[j],width[j]);
                 oscObject->set_paras(j, proposedPars[j],'p');
-                
+
             }
             oscObject->make_sum('p','p',true);
-            
+
         }
         proposedLLH = oscObject->getLLH();
-        
+
         double accProb = TMath::Min(1.,TMath::Exp(currentLLH-proposedLLH));
         double fRandom = rnd->Rndm();
         //std::cout << "i " << i << " accprob " << accProb << " fRandom " << fRandom << " current LLH " << currentLLH << " proposed LLH " << proposedLLH <<  " parameter 1 current: " << currentPars[1] << " parameter 1 proposed:" << proposedPars[1] << std::endl;
-        
+
         if ( fRandom <= accProb )
         {
             for(int k=0; k<currentPars.size(); k++)
@@ -124,44 +123,34 @@ void Markov_Chain::startMH(std::vector<double> currentPars, Nu_Fitter* oscObject
                     currentPars[k]=proposedPars[k];
                     //std::cout << "i " << i << " proposed " << proposedPars[k] <<std::endl;
                 }
-                
+
             }
             currentLLH=proposedLLH;
-            
+
         }
         //std::cout << "i " << i << " Theta23 " << proposedPars[2] <<std::endl;
-        
-        //        outputtree->Branch("Energy",&currentPars[0],"E/D");
-        //        outputtree->Branch("DM2",&currentPars[1],"DM2/D");
-        //        outputtree->Branch("Theta23",&currentPars[2],"Theta23/D");
-        //        outputtree->Branch("Theta13",&currentPars[3],"Theta13/D");
-        //        outputtree->Branch("dm2",&currentPars[4],"dm2/D");
-        //        outputtree->Branch("Theta12",&currentPars[5],"Theta12/D");
-        //        outputtree->Branch("Delta",&currentPars[6],"Delta/D");
-        //        outputtree->Branch("n_series",&currentPars[7],"n_series/D");
-        //        outputtree->Branch("Beta",&currentPars[8],"Beta/D");
-        //        outputtree->Branch("LLH",&currentLLH,"LLH/D");
-        outputtree->Fill();
-        
+
+        tree->Fill();
+
         if(i==steps-1){
             std::cout << "Metropolis-Hastings algorithm completed." << std::endl;
         }
-        
+
     }
-    
-    outputtree->Write();
+
+    tree->Write();
     file->Close();
-    
-    
+
+
 }
 
-void Markov_Chain::startMH(std::vector<double> coef, Disappearance* oscObject, bool dis){
-    currentLLH = oscObject->getLLH();
+void Markov_Chain::startMH(std::vector<double> &currentPars, Disappearance* oscObject, bool dis){
+    /*currentLLH = oscObject->getLLH();
     //int count =0;
-    
+
     for(int i=0; i<steps; i++){
         if(i%1000==0 || i==0){
-            
+
             if(i==0){std::cout << "Initialising random walk..." << std::endl;}
             else{std::cout << "i " << i << "Running random walk..." << std::endl;}
         }
@@ -171,7 +160,7 @@ void Markov_Chain::startMH(std::vector<double> coef, Disappearance* oscObject, b
                 proposedcoef[k]=rnd->Gaus(coef[k],width[k]);
                 oscObject->set_paras_d(k, proposedcoef[k],'p');
             }
-        
+
         }
         oscObject->taylor('p');
         proposedLLH = oscObject->getLLH();
@@ -180,73 +169,73 @@ void Markov_Chain::startMH(std::vector<double> coef, Disappearance* oscObject, b
         //std::cout << "i " << i << " accprob " << accProb << " fRandom " << fRandom << " current LLH " << currentLLH << " proposed LLH " << proposedLLH <<  " parameter 1 current: " << coef[1] << " parameter 1 proposed:" << proposedcoef[1] << std::endl;
          if ( fRandom <= accProb )
         {
-    
+
             for(int m=0; m<coef.size(); m++)
             {
-             
+
                 if(coef_bool[m]){
                     //count++;
                     std::cout << "coef " << m << " " << coef[m] << " proposed " << m << " " << proposedcoef[m] << std::endl;
                     coef[m]=proposedcoef[m];}
-                
+
             }
             currentLLH=proposedLLH;
         }
- 
+
         //std::cout << " 0 " << coef[0] << " 1 " << coef[1] << " 2 " << coef[2]  << std::endl;
-        outputtree->Fill();
-        
-        
+        tree->Fill();
+
+
         if(i==steps-1){
             std::cout << "Metropolis-Hastings algorithm completed." << std::endl;
-        
+
         }
     }
- 
+
    // std::cout << count << std::endl;
-    outputtree->Write();
-    file->Close();
-    
+    tree->Write();
+    file->Close();*/
+
 }
-void Markov_Chain::startMH(std::vector<double> currentPars, Appearance *plusObj, Appearance *minusObj){
-    
-    
-    
+void Markov_Chain::startMH(std::vector<double> &currentPars, Appearance *plusObj, Appearance *minusObj){
+
+
+
     currentLLH = plusObj->getLLH() + minusObj->getLLH(); // add LLH from two files
-    
+
     for(int i=0; i<steps; i++){
-        
+
         if(i%1000==0 || i==0){
-            
+
             if(i==0){
                 std::cout << "Initialising random walk..." << std::endl;
             }
-            
+
             else{
                 std::cout << "Running random walk..." << std::endl;
             }
-            
+
         }
-        
+
         for(int j=0; j<currentPars.size(); j++) {
-            
+
             if(pars[j]){
-                
+
                 proposedPars[j]=rnd->Gaus(currentPars[j],width[j]);
                 plusObj->set_paras(j, proposedPars[j],'p');
                 minusObj->set_paras(j, proposedPars[j],'p');
-                
+
             }
             plusObj->make_sum('p','p',true);
             minusObj->make_sum('p','p',true);
-            
+
         }
         proposedLLH = plusObj->getLLH() + minusObj->getLLH();
-        
+
         double accProb = TMath::Min(1.,TMath::Exp(currentLLH-proposedLLH));
         double fRandom = rnd->Rndm();
-  
-        
+
+
         if ( fRandom <= accProb )
         {
             for(int k=0; k<currentPars.size(); k++)
@@ -255,59 +244,49 @@ void Markov_Chain::startMH(std::vector<double> currentPars, Appearance *plusObj,
                     currentPars[k]=proposedPars[k];
                     //std::cout << "i " << i << " proposed " << proposedPars[k] <<std::endl;
                 }
-                
+
             }
             currentLLH=proposedLLH;
-            
+
         }
         //std::cout << "i " << i << " Theta23 " << proposedPars[2] <<std::endl;
-        
-        //        outputtree->Branch("Energy",&currentPars[0],"E/D");
-        //        outputtree->Branch("DM2",&currentPars[1],"DM2/D");
-        //        outputtree->Branch("Theta23",&currentPars[2],"Theta23/D");
-        //        outputtree->Branch("Theta13",&currentPars[3],"Theta13/D");
-        //        outputtree->Branch("dm2",&currentPars[4],"dm2/D");
-        //        outputtree->Branch("Theta12",&currentPars[5],"Theta12/D");
-        //        outputtree->Branch("Delta",&currentPars[6],"Delta/D");
-        //        outputtree->Branch("n_series",&currentPars[7],"n_series/D");
-        //        outputtree->Branch("Beta",&currentPars[8],"Beta/D");
-        //        outputtree->Branch("LLH",&currentLLH,"LLH/D");
-        outputtree->Fill();
-        
+
+        tree->Fill();
+
         if(i==steps-1){
             std::cout << "Metropolis-Hastings algorithm completed." << std::endl;
         }
-        
+
     }
-    
-    outputtree->Write();
+
+    tree->Write();
     file->Close();
-    
-    
+
+
 }
 
 
 
 void Markov_Chain::set_pars(int index){
-    
+
     pars[index] = true;
-    
+
 }
 
-void Markov_Chain::set_coef(int index){
-    
-    coef_bool[index] = true;
-    
-}
+// void Markov_Chain::set_coef(int index){
+//
+//     coef_bool[index] = true;
+//
+// }
 
 void Markov_Chain::set_width(int index, double value){
-    
+
     width[index] = value;
-    
+
 }
 
 void Markov_Chain::print(){
-    
+
     std::cout << "Proposed parameters: " << std::endl
     << "Energy\t "   << proposedPars[0] << std::endl
     << "DM2\t "      << proposedPars[1] << std::endl
@@ -318,6 +297,5 @@ void Markov_Chain::print(){
     << "Delta\t "    << proposedPars[6] << std::endl
     << "n_series " << proposedPars[7] << std::endl
     << "Beta\t "     << proposedPars[8] << std::endl;
-    
-}
 
+}
--- Markov_Chain.hpp
+++ Markov_Chain.hpp
@@ -9,15 +9,15 @@
 #ifndef Markov_Chain_hpp
 #define Markov_Chain_hpp
 
-#include <stdio.h>
 #include "Nu_Fitter.hpp"
 #include "Disappearance.hpp"
 #include "Appearance.h"
-#include "Appearance.cpp"
+// #include "Appearance.cpp"
 #include "TFile.h"
 #include "TH1D.h"
 #include "TApplication.h"
 
+#include <stdio.h>
 #include <cstdlib>
 #include <ctime>
 #include <string>
@@ -36,39 +36,41 @@
 #include "TMath.h"
 #include "TRandom3.h"
 #include "TTree.h"
+#include "TString.h"
 
 
 
 class Markov_Chain{
-    
+
 public:
-    Markov_Chain(std::vector<double> currentPars, std::vector<double> coef, int nstep, std::string filename);
+    Markov_Chain(std::vector<double> &obj_pars, std::vector<std::string> &parsName, int nstep, std::string filename);
     ~Markov_Chain();
-    void startMH(std::vector<double> currentPars, Nu_Fitter* oscObject); // starts the Metropolis-Hastings algorithm
-    void startMH(std::vector<double> coef, Disappearance* oscObject, bool dis);
-    void startMH(std::vector<double> currentPars, Appearance *plusObj, Appearance *minusObj);
+    void startMH(std::vector<double> &currentPars, Nu_Fitter *oscObject); // starts the Metropolis-Hastings algorithm
+    void startMH(std::vector<double> &currentPars, Disappearance *oscObject, bool dis);
+    void startMH(std::vector<double> &currentPars, Appearance *plusObj, Appearance *minusObj);
     void set_pars(int index); // if element is true, the parameter associated with the true element is varied in the Markov chain.
     void set_width(int index, double value);// fine tunes the width of a particular parameter
     void print(); // prints the proposed parameters
-    void set_coef(int index);
-    
+    // void set_coef(int index);
+
 private:
-    
+
     int steps;
     std::vector<double> proposedPars;
-    std::vector<double> proposedcoef;
+    // std::vector<double> proposedcoef;
 
     double currentLLH;
     double proposedLLH;
     std::vector<bool> pars;
-    std::vector<bool> coef_bool;
+    std::vector<bool> pars_bool;
     TFile* file;
-    TTree* outputtree;
+    TTree* tree;
     std::vector<double> width;
     TRandom3* rnd;
-    
-    
-    
+    std::vector<std::string> branchName;
+
+
+
 };
 
 
