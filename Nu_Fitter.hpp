6e6bbfd686cb6c98867c6971cddf0031423a847d
AndrewYongZhenNing <ayzn95@gmail.com>
2017-06-25 19:05:33 +0100

    includes sigma_cc and improved make_sum

--- Nu_Fitter.hpp
+++ Nu_Fitter.hpp
@@ -9,20 +9,17 @@
 #ifndef Nu_Fitter_hpp
 #define Nu_Fitter_hpp
 
-#include <math.h>
-#include <iostream>
-#include <fstream>
-
 #include "BargerPropagator.h"
 
-#include "TFile.h"
-#include "TH1D.h"
-#include "TApplication.h"
-
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <math.h>
 #include <cstdlib>
 #include <ctime>
 #include <string>
 #include <sstream>
+#include <vector>
 
 #include "TROOT.h"
 #include "TStyle.h"
@@ -36,38 +33,41 @@
 #include "TF2.h"
 #include "TMath.h"
 #include "TRandom3.h"
+#include "TH1D.h"
+#include "TApplication.h"
 
-#include <stdio.h>
-#include <vector>
-#include <string>
-using namespace std;
 
 class Nu_Fitter{
-    
+
 public:
-    
+
     Nu_Fitter(int kNuBarVar, std::string path, std::string filename1, std::string filename2, std::string filename3, std::string filename4); // overloaded constructor: takes data from two files instead
     ~Nu_Fitter();
-    
-    void make_sum(char hist_type, char vector_type, bool oscillate);
+
+    void make_sum(char hist_type, bool oscillate);
     // applies probability to each raw histogram and then sums them up.
-    // Command: hist_type: 'd' for (fake) data, 'p' for prediction; vector_type: 'c' for current parameters; 'p' for proposed parameters; oscillate: true for oscillation, false for no oscillation
-    
+    // Command: hist_type: 'd' for (fake) data, 'p' for prediction; oscillate: true for oscillation, false for no oscillation
+
     void print_kNu(); //tells user what value kNuBar has at the moment
+
     double getLLH(); // calculates the log likelihood
+
     void show_Prediction(); // show the prediction histogram for analysis
-    std::vector<double> return_cparam();
+
+    std::vector<double> return_cparam(); // return currentsPars
+
+    std::vector<std::string> return_sparam(); // return parsName
+
     void set_paras(int index, double val, char vector_type);
     //redefine the value of the index'th parameter
     //Command: vector_type: 'c' to set currentPars, 'p' to set proposedPars
-    
+
+    double sigma_cc(int nu_mode, double bin_E);
+    // returns the pre-factor when taking into account interaction cross-section
+    //Command: 1: nue; -1: nueb; 2: numu; -2:numub
+
 protected:
-    TH1D* _Data;
-    TH1D* _input1;// nu mu
-    TH1D* _input2;// nu mu bar
-    TH1D* _input3;// nu e
-    TH1D* _input4;// nu e bar
-    TH1D* _Prediction;
+
     bool kSquared;   // using sin^2(x) variables?
     int kNuBar; // positive for neutrino, negative for antineutrino
     double BasePath; //km
@@ -78,11 +78,19 @@ protected:
     std::string _filename2;
     std::string _filename3;
     std::string _filename4;
-    vector<double> currentPars;
-    vector<double> proposedPars;
-    
-    
-    
+    std::vector<double> currentPars;
+    std::vector<double> proposedPars;
+    std::vector<std::string> parsName;
+
+    TH1D* _Data;
+    TH1D* _input1;// nu mu
+    TH1D* _input2;// nu mu bar
+    TH1D* _input3;// nu e
+    TH1D* _input4;// nu e bar
+    TH1D* _Prediction;
+
+
+
 };
 
 
