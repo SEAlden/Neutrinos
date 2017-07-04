698e49ae66dad8b3f07db89138aee8d2abb680b0
Andrew Yong <ayzn95@gmail.com>
2017-07-04 15:27:48 +0100

    boundPlot macro first commit

---
+++ boundPlot.cpp
@@ -0,0 +1,38 @@
+#include "BargerPropagator.h"
+
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <math.h>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <sstream>
+#include <vector>
+
+#include "TROOT.h"
+#include "TStyle.h"
+#include "TString.h"
+#include "TCanvas.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TFile.h"
+#include "TNtuple.h"
+#include "TF1.h"
+#include "TF2.h"
+#include "TMath.h"
+#include "TRandom3.h"
+#include "TH1D.h"
+#include "TApplication.h"
+
+void boundPlot(const char* filename, std::string parameter){
+
+  if(a==1){
+    std::cout << "Hello world!" << std::endl;
+  }
+  else{
+    std::cout <<"blep" << std::endl;
+  }
+
+
+}
