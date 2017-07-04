db66a161f0649cdfbf97a399c461de16af22f3e9
Andrew Yong <ayzn95@gmail.com>
2017-07-04 21:24:35 +0100

    Working boundPlot

--- boundPlot.cpp
+++ boundPlot.cpp
@@ -2,37 +2,67 @@
 
 #include <stdio.h>
 #include <iostream>
-#include <fstream>
-#include <math.h>
-#include <cstdlib>
 #include <ctime>
 #include <string>
 #include <sstream>
 #include <vector>
 
-#include "TROOT.h"
-#include "TStyle.h"
 #include "TString.h"
-#include "TCanvas.h"
-#include "TH1.h"
-#include "TH2.h"
 #include "TFile.h"
-#include "TNtuple.h"
-#include "TF1.h"
-#include "TF2.h"
 #include "TMath.h"
-#include "TRandom3.h"
 #include "TH1D.h"
-#include "TApplication.h"
 
-void boundPlot(const char* filename, std::string parameter){
+void boundPlot(TString filename, TString parameter,int resolution, double xmin, double xmax){
 
-  if(a==1){
-    std::cout << "Hello world!" << std::endl;
+  TFile* file = new TFile(filename);
+  TTree* tree = (TTree*)file->Get("t");
+
+  TString hname = "h";
+  hname+=parameter;
+  TH1D* hist = new TH1D(hname,parameter,resolution,xmin,xmax); // 400 gives about the same resolution as in the TTree plot
+
+  tree->Project(hname,parameter);
+  hist->DrawCopy();
+
+  double percentage = 0;
+  int i = 1;
+  int maxbin = hist->GetMaximumBin();
+
+  while(percentage<90){
+
+    if(maxbin-i > 0){
+      percentage = 100*(hist->Integral(maxbin-i,maxbin+i))/(hist->Integral());
+    }
+
+    else{
+      percentage = 100*(hist->Integral(0,maxbin+i))/(hist->Integral());
+    }
+
+    i++;
+
   }
-  else{
-    std::cout <<"blep" << std::endl;
-  }
+
+    hist->SetFillColor(9);
+    hist->GetXaxis()->SetRange(maxbin-i,maxbin+i);
+    hist->DrawCopy("same");
 
 
+
+
+  // FILL BIN WITH COLOUR
+  //maxbin = GetMaximumBin() // gets the maximum weight in the histogram
+  //GetXaxis()->GetBinCenter(maxbin) // gets the bin number that has the maximum bin
+
+  std::cout << "Distribution bound." << std::endl;
 }
+
+// void boundPlot() {
+//    TH1F *h = new TH1F("h","a colored plot",100,0,3);
+//    h->FillRandom("gaus",10000);
+//    h->DrawCopy();
+//    for (Int_t i=1;i<100;i+=5) {
+//       h->SetFillColor((i+4)/5);
+//       h->GetXaxis()->SetRange(i,i+4);
+//       h->DrawCopy("same");
+//    }
+// }
